62e0697bf50c74194a21dacbbdb948fff26304e0
Thomas L <Cubey2019@users.noreply.github.com>
2018-02-05 11:53:52 +0000

    P2P service now applies the witness flag when requesting data for new blocks/transactions.

--- test/services/p2p/index.unit.js
+++ test/services/p2p/index.unit.js
@@ -4,11 +4,12 @@ var expect = require('chai').expect;
 var sinon = require('sinon');
 var EventEmitter = require('events').EventEmitter;
 var P2PService = require('../../../lib/services/p2p');
+var Inventory = require('vertcore-p2p').Inventory;
 
 describe('P2P Service', function() {
   var p2p;
   var testEmitter;
-
+  
   before(function(done) {
     p2p = new P2PService({
       node: {
@@ -28,6 +29,29 @@ describe('P2P Service', function() {
     p2p.getMempool();
     expect(getPeer.calledOnce).to.be.true;
     expect(sendMessage.calledOnce).to.be.true;
+  });
+
+  it('should request witness blocks', function() {    var sendMessage = sinon.stub();
+    var sendMessage = sinon.stub();
+    var peer = { sendMessage: sendMessage };
+    var invMessage = {
+      inventory: [
+        { hash: '0123456789abcdef1', type: Inventory.TYPE.BLOCK },
+        { hash: '0123456789abcdef2', type: Inventory.TYPE.TX },
+        { hash: '0123456789abcdef3', type: Inventory.TYPE.FILTERED_BLOCK },
+        { hash: '0123456789abcdef4', type: Inventory.TYPE.CMPCT_BLOCK }
+      ]
+    }
+    p2p._initCache();
+    p2p._onPeerInventory(peer, invMessage);
+    expect(sendMessage.calledOnce).to.be.true;
+    expect(sendMessage.getCall(0).args.length).to.equal(1);
+    expect(sendMessage.getCall(0).args[0].inventory).to.deep.equal([
+      { hash: '0123456789abcdef1', type: Inventory.TYPE.WITNESS_BLOCK },
+      { hash: '0123456789abcdef2', type: Inventory.TYPE.WITNESS_TX },
+      { hash: '0123456789abcdef3', type: Inventory.TYPE.FILTERED_WITNESS_BLOCK },
+      { hash: '0123456789abcdef4', type: Inventory.TYPE.CMPCT_BLOCK }
+    ]);
   });
 });
 
